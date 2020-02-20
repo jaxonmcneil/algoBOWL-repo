86f50dc121ad5297a23dbd9094f3424ac97df442
jaxonmcneil <jaxonmcneil@mymail.mines.edu>
2020-02-19 23:52:55 -0700

    added heuristic.cpp

---
+++ heuristic.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jaxon on 2/19/2020.
+//
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void generateCombos(int num, vector<string> combos){
+
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "heuristic.cpp"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
