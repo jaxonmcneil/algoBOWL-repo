a18dc7c9acc64c07e438259755c7bb7bf4c7116d
jaxonmcneil <jaxonmcneil@mymail.mines.edu>
2020-02-20 19:44:15 -0700

    added heuristic2.cpp containing a more optimal/elegant approach to the solution to the problem, code is unfinished (see TODO for next part)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <time.h>
 #include "heuristic.cpp"
+#include "heuristic2.cpp"
 #include "randomFileGenerator.cpp"
 
 using namespace std;
@@ -8,10 +9,10 @@ using namespace std;
 int main() {
     srand(time(NULL));
 
-    const int M(50), N(10);
+    const int M(1), N(15);
 
-    randomFileGenerator("samplein.txt", M, N);
-    bruteForceHeuristic("samplein.txt");
+    //randomFileGenerator("samplein.txt", M, N);
+    optimizedHeuristic("samplein.txt");
 
     return 0;
 }
